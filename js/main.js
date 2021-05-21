8341c73160e727599dafc42aa61fa9f51553627d
Engels Baca Sevilla <engelsbaca@spartansv.net>
2021-05-20 18:24:14 -0600

    Login page added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 import { getContainerSPA, getPage } from './util.js'
 
 const main = getContainerSPA();
-const page = await getPage("../pages/main.html");
-main.innerHTML = page;
+
+getPage("../pages/main.html").then((response) => {
+    main.innerHTML = response
+})
+
+const accountBtn = document.querySelector('#accountBtn')
+
+accountBtn.addEventListener('click', (event) => {
+    getPage("../pages/login.html").then((response) => {
+        main.innerHTML = response
+        console.log('test')
+    })
+})
